7ab1a413a050dc3d304d98a0456e5c08eb6a0498
Azanul Rana <97664456+0xab01@users.noreply.github.com>
2023-12-27 06:52:36 -0800

    Add files via upload

---
+++ src/data/index.js
@@ -0,0 +1,49 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Shadan',
+    lastName: 'Khan',
+    email: 'shadankhan@example.com',
+    salary: '95000',
+    date: '2019-04-11',
+  },
+  {
+    id: 2,
+    firstName: 'Azanul',
+    lastName: 'Rana',
+    email: 'azanulrana@example.com',
+    salary: '80000',
+    date: '2019-04-17',
+  },
+  {
+    id: 3,
+    firstName: 'Nizamul',
+    lastName: 'Rana',
+    email: 'nizamulrana@example.com',
+    salary: '79000',
+    date: '2019-05-01',
+  },
+ 
+  {
+    id: 5,
+    firstName: 'Anzal',
+    lastName: 'Khan',
+    email: 'anzalkhan@example.com',
+    salary: '65000',
+    date: '2019-06-13',
+  },
+ 
+  {
+    id: 7,
+    firstName: 'Taha',
+    lastName: 'N',
+    email: 'TahaN@example.com',
+    salary: '90000',
+    date: '2019-08-15',
+  },
+
+
+  
+];
+
+export { employeesData };
